addcd03f6973ad4daa28361b4036ab5245ca163f
Stefano Scipioni <scipio.it@gmail.com>
2021-05-02 07:53:15 +0200

    .

--- keymap.c
+++ keymap.c
@@ -1,51 +1,19 @@
 #include QMK_KEYBOARD_H
 
 
-// #ifdef RGBLIGHT_ENABLE
-// //Following line allows macro to read current RGB settings
-// extern rgblight_config_t rgblight_config;
-// #endif
-
-// extern uint8_t is_master;
-
-// // Each layer gets a name for readability, which is then used in the keymap matrix below.
-// // The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// // Layer names don't all need to be of the same length, obviously, and you can also skip them
-// // entirely and just use numbers.
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
 
-// enum custom_keycodes {
-//   QWERTY = SAFE_RANGE,
-//   LOWER,
-//   RAISE,
-//   ADJUST,
-//   BACKLIT,
-//   RGBRST
-// };
-
-// enum macro_keycodes {
-//   KC_SAMPLEMACRO,
-// };
-
-//#define LY_LWR MO(_LOWER)
-//#define LY_RSE MO(_RAISE)
-
-// ESC when tapped and Left Control when held  (see TAPPING_TERM in config.h)
-#define ESC_LCTL LCTL_T(KC_ESC)
-#define MINS_RCTL LCTL_T(KC_MINS)
-// SUPER when tapped and Left ALT when held
-//#define KY_SUPER_LALT ALT_T(KC_LGUI)
 
 // Mod-Tap key MT(mod, kc) acts like a modifier when held, and a regular keycode kc when tapped
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3( \
-       KC_GRV,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
-       ESC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, MINS_RCTL,\
-       KC_TAB,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_QUOTE,\
+       MT(MOD_LGUI, KC_GRV),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
+       MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, MT(MOD_RCTL, KC_MINS),\
+       MT(MOD_LGUI, KC_TAB),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_QUOTE,\
                             MT(MOD_LALT, KC_LGUI), KC_LSFT,LT(_LOWER,KC_SPC),\
                                                                       LT(_RAISE, KC_ENT), KC_RSFT,  KC_ALGR \
   ),
